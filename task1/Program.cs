fd8e41a0b2c0c3ffb6af4c772c1daed858558c9f
Andrey Mikheev <athlon26@mail.ru>
2025-03-05 14:56:30 +0300

    add seminar2:task1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(ReadLine());
+if (a >= 99 && a <= 99)
+{
+int digit1 = a/100;
+int digit3 = a%10;
+Console.Write(digit1+digit3);
+}
+else
+{
+    Console.Write("Ошибка! Это не трехзначное число!");
+}
