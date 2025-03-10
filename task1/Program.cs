dbae852c8a750ff9100df88c1f3df24d8f54edfb
Andrey Mikheev <athlon26@mail.ru>
2025-03-10 13:13:40 +0300

    add changes:task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -8,11 +8,13 @@ Console.Write("Введите трехзначное число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 if (a >= 100 && a <= 999)
 {
-int digit1 = a/100;
-int digit3 = a%10;
-Console.Write($"{digit1}{digit3}");
+    int digit1 = a / 100;
+    int digit3 = a % 10;
+    int result = digit1 * 10 + digit3;
+//Console.Write($"{digit1}{digit3}");
+Console.WriteLine(result);
 }
 else
 {
-Console.Write("Ошибка! Это не трехзначное число!");
+    Console.Write("Некоректный ввод!");
 }
